5a17cb6cd933ebc16b1ff2a555d36ad087fecbeb
prathishaprathisha <41039872+prathishaprathisha@users.noreply.github.com>
2018-12-27 14:42:26 +0530

    Add files via upload

---
+++ EXe50.py
@@ -0,0 +1,9 @@
+def isPowerOfTwo(m):
+    if (m == 0):return False
+    while (m != 1):
+        if (m % 2 != 0):return False
+        m = m // 2
+    return True
+m=int(input("Enter Value"))
+if(isPowerOfTwo(n)):print('yes')
+else:print('no')
