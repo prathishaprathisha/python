ac95b1ce489ca9417ad74457940f264be311d171
prathishaprathisha <41039872+prathishaprathisha@users.noreply.github.com>
2018-12-28 14:33:12 +0530

    Add files via upload

---
+++ Exe106.py
@@ -0,0 +1,8 @@
+y=int(input("enter the n"))
+x=[]
+z=int(input("enter the n to find odd"))
+for i in range(0,y):
+    c=int(input("enter the number"))
+    if(c%2==1):
+        x.append(c)
+print(x[e-1])
