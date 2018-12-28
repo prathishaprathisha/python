b6a09d9201a295080af78c2ecb1cc88c7f371420
prathishaprathisha <41039872+prathishaprathisha@users.noreply.github.com>
2018-12-28 14:40:23 +0530

    Add files via upload

---
+++ Exe108.py
@@ -0,0 +1,7 @@
+p=int(input("enter the n"))
+l=[]
+s=int(input("enter the n to find nth smallest no"))
+for i in range(0,p):
+    s=int(input("enter the number"))
+    l.append(s)
+print(sorted(l)[e-1])
